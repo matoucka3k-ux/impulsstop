c74f760d4a386ea7f0f0b5977082076628d3e1f1
matoucka3k-ux <matoucka3k@gmail.com>
2026-03-06 16:15:15 +0100

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,1 +1,7 @@
 
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = 'https://uqolnnpsjezrqkmtdqqi.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxb2xubnBzamV6cnFrbXRkcXFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MDkxMDksImV4cCI6MjA4ODM4NTEwOX0.gv0PD-KzmdrxSq5gE1hxbkIdWnUod_JdeMud261YTlc';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
